31d69b15f235bf39d0ab18cb8cb99982cf1df5af
alexander lisovich <lisovyhs@gmail.com>
2025-09-19 06:41:33 +0300

    added User entity, service and repository

---
+++ src/main/java/by/lisovich/binance_finance_tracker/entity/User.java
@@ -0,0 +1,32 @@
+package by.lisovich.binance_finance_tracker.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "username", unique = true, nullable = false)
+    private String username;
+
+    @Column(name = "password_hash", nullable = false)
+    private String passwordHash;
+
+    @Column(name = "email", nullable = false)
+    private String email;
+
+    @Column(name = "binance_api_key")
+    private String binanceApiKey;
+
+    @Column(name = "binance_secret_key")
+    private String binanceSecretKey;
+}
