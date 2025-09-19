b5372f9c0fe786b3074cf820870d1b5d00f6d323
alexander lisovich <lisovyhs@gmail.com>
2025-09-19 05:46:45 +0300

    added initial project

---
+++ src/main/java/by/lisovich/binance_finance_tracker/BinanceFinanceTrackerApplication.java
@@ -0,0 +1,13 @@
+package by.lisovich.binance_finance_tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BinanceFinanceTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BinanceFinanceTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/by/lisovich/binance_finance_tracker/BinanceFinanceTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package by.lisovich.binance_finance_tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BinanceFinanceTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
