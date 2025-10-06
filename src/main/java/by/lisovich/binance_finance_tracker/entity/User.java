64104b4295f5dfb5ad37a7312305dfca14e27cb3
alexander lisovich <lisovyhs@gmail.com>
2025-10-06 08:11:05 +0300

    tested unit UserService class

--- src/main/java/by/lisovich/binance_finance_tracker/entity/User.java
+++ src/main/java/by/lisovich/binance_finance_tracker/entity/User.java
@@ -2,6 +2,7 @@ package by.lisovich.binance_finance_tracker.entity;
 
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.CreationTimestamp;
@@ -12,6 +13,7 @@ import java.time.LocalDateTime;
 @Entity
 @Table(name = "users")
 @Data
+@Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class User {
--- src/main/java/by/lisovich/binance_finance_tracker/service/UserService.java
+++ src/main/java/by/lisovich/binance_finance_tracker/service/UserService.java
@@ -1,7 +1,7 @@
 package by.lisovich.binance_finance_tracker.service;
 
+import by.lisovich.binance_finance_tracker.entity.User;
 import by.lisovich.binance_finance_tracker.repository.UserRepository;
-import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -11,4 +11,11 @@ public class UserService {
 
     private final UserRepository userRepository;
 
+    public User findByEmail(String email) {
+        return userRepository.findByEmail(email).orElseThrow(() -> new IllegalStateException("User is not exist"));
+    }
+
+    public User save(User embedUser) {
+        return userRepository.save(embedUser);
+    }
 }
